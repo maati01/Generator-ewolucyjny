6ff8ee1b6eda7b28fba26a4e138094db9f3917da
maati01 <mateuszpow7@gmail.com>
2021-12-23 22:25:10 +0100

    Create Main.java

---
+++ src/main/java/agh/ics/generator/Main.java
@@ -0,0 +1,11 @@
+package agh.ics.generator;
+
+import agh.ics.generator.gui.App;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+
+}
